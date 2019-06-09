cd8378f242af1eacd8d131bea5136d7947668d19
Desteel <30462928+Desteel@users.noreply.github.com>
2019-06-09 22:36:27 +0400

    update

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,17 +1,28 @@
 import { observer } from 'mobx-react';
 import React from 'react';
 
+import TestModel from '../models/Test';
+
 const Select = ({ group, handleChange }) => {
     return (
-        <select onChange={handleChange}>
-            <option>- select user -</option>
+        <>
+            <button
+                onClick={() => {
+                    console.log(TestModel.name);
+                }}
+            >
+                test log
+            </button>
+            <select onChange={handleChange}>
+                <option>- select user -</option>
 
-            {[...group.users.values()].map(user => (
-                <option key={user.id} value={user.id}>
-                    {user.name}
-                </option>
-            ))}
-        </select>
+                {[...group.users.values()].map(user => (
+                    <option key={user.id} value={user.id}>
+                        {user.name}
+                    </option>
+                ))}
+            </select>
+        </>
     );
 };
 
